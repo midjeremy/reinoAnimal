c846600f6b2302e891e8b9f2986201bed1f62cd5
midjeremy <jeremyampaimorales@gmail.com>
2024-10-09 19:10:58 -0300

    #

--- reinoAnimal/settings.py
+++ reinoAnimal/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'l;kasdl;kasd;lkasd;lasd;lk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1'.split(',')]
+ALLOWED_HOSTS = ['127.0.0.1, reinoanimal.onrender.com'.split(',')]
 
 
 
